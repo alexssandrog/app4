d667dd05bac18617203f35d286069246528d454d
Alexssandro Gottschalk <alex@gottschalk.com.br>
2018-04-02 17:57:52 -0300

    Fim da aula 113

---
+++ src/components/ListaItens.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class ListaItens extends Component {
+  render() {
+      console.log('Feitoria');
+    return (
+      <Text>Testando 1 2 3</Text>
+    );
+  }
+}
